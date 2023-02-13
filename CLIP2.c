ce250f341297d45f02f88b3c26fd3014a824ac10
MonnaDang <chuongmap0979511903@gmail.com>
2023-02-13 10:02:00 +0700

    Chuong Trinh Cua Clip 2

--- CLIP2.c
+++ CLIP2.c
@@ -1,90 +1,88 @@
-
+//!#define   BOARD     D501
 #include<tv_boards.c>  
-unsigned int8 i,j,mode=1,toc_do=1;
+
+unsigned int8 i,j;
+unsigned int8 che_do = 1, cap_tocdo = 1;
 unsigned int16 k,speed=50;
-int1 doi_mode;
-/*
-   doi_mode = 0 => doi hieu ung led
-   doi_mode = 1 => doi thoi gian delay
-   10 cap: 50 -> 500 ms
-*/
+int1 tt;
+unsigned int16 Up_state,Dw_state,Mod_state;
 
-void my_delay(){
+void kt_nutnhan(){
    delay_ms(1);
-   if(!inputcd(UP)){
-      if(!doi_mode)
-         if(mode == 10) mode=10;
-         else mode++;
-      else 
-         if(toc_do == 10) toc_do = 10 ;
-         else {
-            toc_do++;
-            speed = (int16)toc_do*50;
-         }
+   Up_state = (Up_state<<1) | input(UP);
+   Dw_state = (Dw_state<<1) | input(DW);
+   Mod_state = (Mod_state<<1) | input(MOD);
+//!   if(!input(UP)){
+   if(Up_state == 0x8000){
+      if(!tt){
+         if(che_do < 10)che_do++;
+      }else{
+         if(cap_tocdo < 10)cap_tocdo++;
+         speed = (int16)cap_tocdo*50;
+      }
    }
-   if(!inputcd(DW)){
-      if(!doi_mode)
-         if(mode == 1) mode=1;
-         else mode--;
-      else 
-         if(toc_do == 1) toc_do = 1 ;
-         else toc_do--;
-         speed = (int16)toc_do*50;
+//!   if(!input(DW)){
+   if(Dw_state == 0x8000){
+      if(!tt){
+         if(che_do > 1)che_do--;
+      }else{
+         if(cap_tocdo > 1)cap_tocdo--;
+         speed = (int16)cap_tocdo*50;
+      }
    }
-   if(!inputcd(MOD)){
-      doi_mode = !doi_mode;
+//!   if(!input(MOD)){
+   if(Mod_state == 0x8000){
+      tt = !tt;
    }
-   
 }
-
 
 void SangDan_TSP(){
    for(i=0;i<=24;i++){
-      for(k=0;k<speed;k++) my_delay();
-      led32.ledx32 = (~(0x0fffffff>>i)) & 0x0ffffff0 | (int32)doi_mode;
+      led32.ledx32 = (~(0x0fffffff>>i)) & 0x0ffffff0 | (int32)tt;
       led32_display();
+      for(k=0;k<speed;k++)kt_nutnhan();
    }
 }
 
 void SangDan_PST(){
    for(i=0;i<=24;i++){
-      for(k=0;k<speed;k++) my_delay();
-      led32.ledx32 = (~(0xfffffff0<<i)) & 0x0ffffff0 | (int32)doi_mode;
+      led32.ledx32 = (~(0xfffffff0<<i)) & 0x0ffffff0 | (int32)tt;
       led32_display();
+      for(k=0;k<speed;k++)kt_nutnhan();
    }
 }
 
 void ChopTat_5lan(){
    for(i=0;i<10;i++){
-      for(k=0;k<speed;k++) my_delay();
-      led32.ledx32 = (~led32.ledx32) & 0x0ffffff0 | (int32)doi_mode;
+      led32.ledx32 = (~led32.ledx32) & 0x0ffffff0 | (int32)tt;
       led32_display();
+      for(k=0;k<speed;k++)kt_nutnhan();
    }
 }
 
 void SangDich_TSP(){
    for(i=0;i<24;i++){
-      for(k=0;k<speed;k++) my_delay();
-      led32.ledx32 = (0x08000000>>i) | (int32)doi_mode;
+      led32.ledx32 = 0x08000000>>i | (int32)tt;
       led32_display();
+      for(k=0;k<speed;k++)kt_nutnhan();
    }
 }
 
 void SangDich_PST(){
    for(i=0;i<24;i++){
-      for(k=0;k<speed;k++) my_delay();
-      led32.ledx32 = ((int32)0x00000010<<i) | (int32)doi_mode;
+      led32.ledx32 = (int32)0x00000010<<i | (int32)tt;
       led32_display();
+      for(k=0;k<speed;k++)kt_nutnhan();
    }
 }
 
 void SangDan_ChopTat(){
    led32.ledx16[1] = 0;
    for(i=0;i<=12;i++){
-      for(k=0;k<speed;k++) my_delay();
-      led32.ledx16[0] = (~(0xfff0<<i)) & 0xfff0 | (int16)doi_mode;
+      led32.ledx16[0] = (~(0xfff0<<i)) & 0xfff0 | (int16)tt;
       led32.ledx16[1] = (~led32.ledx16[1]) & 0x0fff;
       led32_display();
+      for(k=0;k<speed;k++)kt_nutnhan();
    }
 }
 
@@ -92,10 +90,9 @@ void SangDan_ChopTat(){
 void SangDon_TSP(){
    for(i=0;i<=24;i++){
       for(j=0;j<24-i;j++){
-         for(k=0;k<speed;k++) my_delay();
-         led32.ledx32 = ((~(0x0fffffff>>i))|((int32)0x00000010<<j)) & 0x0ffffff0 | (int32)doi_mode;
+         led32.ledx32 = ((~(0x0fffffff>>i))|((int32)0x00000010<<j)) & 0x0ffffff0 | (int32)tt;
          led32_display();
- 
+         for(k=0;k<speed;k++)kt_nutnhan();
       }
    }
 }
@@ -103,9 +100,9 @@ void SangDon_TSP(){
 void SangDon_PST(){
    for(i=0;i<=24;i++){
       for(j=0;j<24-i;j++){
-         for(k=0;k<speed;k++) my_delay();
-         led32.ledx32 = ((~(0xfffffff0<<i))|((int32)0x08000000>>j)) & 0x0ffffff0 | (int32)doi_mode;
+         led32.ledx32 = ((~(0xfffffff0<<i))|(0x08000000>>j)) & 0x0ffffff0 | (int32)tt;
          led32_display();
+         for(k=0;k<speed;k++)kt_nutnhan();
       }
    }
 }
@@ -113,18 +110,18 @@ void SangDon_PST(){
 void ChopTatXemKe_5lan(){
    led32.ledx32 = 0x55555555;
    for(i=0;i<10;i++){
-      for(k=0;k<speed;k++) my_delay();
-      led32.ledx32 = (~led32.ledx32) & 0x0ffffff0 | (int32)doi_mode;
+      led32.ledx32 = (~led32.ledx32) & 0x0ffffff0 | (int32)tt;
       led32_display();
+      for(k=0;k<speed;k++)kt_nutnhan();
    }
 }
 
 void SangDanTrongRaNgoai(){
    for(i=0;i<=12;i++){
-      for(k=0;k<speed;k++) my_delay();
-      led32.ledx16[0] = (~(0xffff>>i)) & 0xfff0 | (int16)doi_mode;
-      led32.ledx16[1] = (~(0xffff<<i)) & 0x0fff;
+      led32.ledx16[0] = (~(0xffff>>i))&0xfff0 | (int16)tt;
+      led32.ledx16[1] = (~(0xffff<<i))&0x0fff;
       led32_display();
+      for(k=0;k<speed;k++)kt_nutnhan();
    }
 }
 
@@ -133,16 +130,15 @@ void main()
       system_init();
       while(true)
       {     
-         if(mode == 1)      SangDan_TSP();
-         else if(mode == 2) SangDan_PST();
-         else if(mode == 3) ChopTat_5lan();
-         else if(mode == 4) SangDich_TSP();
-         else if(mode == 5) SangDich_PST();
-         else if(mode == 6) SangDan_ChopTat();
-         else if(mode == 7) SangDon_TSP();
-         else if(mode == 8) SangDon_PST();
-         else if(mode == 9) ChopTatXemKe_5lan();
-         else if(mode == 10) SangDanTrongRaNgoai();
-
+         if(che_do == 1)       SangDan_TSP();
+         else if(che_do == 2)  SangDan_PST();
+         else if(che_do == 3)  ChopTat_5lan();
+         else if(che_do == 4)  SangDich_TSP();
+         else if(che_do == 5)  SangDich_PST();
+         else if(che_do == 6)  SangDan_ChopTat();
+         else if(che_do == 7)  SangDon_TSP();
+         else if(che_do == 8)  SangDon_PST();
+         else if(che_do == 9)  ChopTatXemKe_5lan();
+         else if(che_do == 10) SangDanTrongRaNgoai();
       }
 }
